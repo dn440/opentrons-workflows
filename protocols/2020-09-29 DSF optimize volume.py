49c59687558699fa5405be098b243216a3d897ca
unknown <Admin@WKS045162.workspace.rug.nl>
2020-09-30 10:44:40 +0200

    test 384 well protocol with only using p10 tips to optimize volume to 15uL in DSF assay

--- protocols/2020-09-29 DSF optimize volume.py
+++ protocols/2020-09-29 DSF optimize volume.py
@@ -11,6 +11,8 @@
 # Therefore this protocol serves the optimization of pipetting accuracy and liquid adhesion of small volumes into a 384-well plate.
 # Methylene blue can be used to make visual examination easier.
 
+# Notes: do not use touch tip, this guarantees losing tips
+
 from opentrons import protocol_api
 
 metadata = {'apiLevel': '2.5'}
@@ -42,13 +44,13 @@ def run(protocol: protocol_api.ProtocolContext):
     p50.well_bottom_clearance.dispense = 7
 
     # Specify target wells (ONLY NEED TO EDIT WELLS HERE)
-    samples = ["7", "8", "9"] # columns on sample plate with protein and dye
+    samples = ["1", "2", "3"] # columns on sample plate with protein and dye
     cols_compounds = [6, 7, 8, 9, 10] # library columns to aspirate compounds from
-    cols_woSAM = [1, 2, 3, 4, 5] # destination wells in reaction plate (384-tandem)
+    cols_woSAM = [1, 2, 3] # destination wells in reaction plate (384-tandem)
     cols_wSAM = [6, 7, 8, 9, 10] # destination wells in reaction plate (384-tandem) (30 uM SAM final c)
-    wells_reaction_woSAM = ['B' + str(i) for i in cols_woSAM]
-    wells_reaction_wSAM = ['B' + str(i) for i in cols_wSAM]
-    wells_reaction = ['B' + str(i) for i in (cols_woSAM + cols_wSAM)]
+    wells_reaction_woSAM = ['A' + str(i) for i in cols_woSAM]
+    wells_reaction_wSAM = ['A' + str(i) for i in cols_wSAM]
+    wells_reaction = ['A' + str(i) for i in (cols_woSAM + cols_wSAM)]
     
     #######################################################################################################################################
     ## PROCEDURE
@@ -58,14 +60,17 @@ def run(protocol: protocol_api.ProtocolContext):
     # disposal_volume = 0, blow_out = False, touch_tip = True)
 
     # Distribute protein mixed with fluorescent dye
-    p10.distribute(13.5, sample_plate.columns_by_name()[samples[0]], [reaction_plate.wells_by_name()[i] for i in wells_reaction_woSAM],
-    disposal_volume = 0, blow_out = False, new_tip = 'once', touch_tip = True)
-    p10.distribute(13.5, sample_plate.columns_by_name()[samples[1]], [reaction_plate.wells_by_name()[i] for i in wells_reaction_wSAM],
-    disposal_volume = 0, blow_out = False, new_tip = 'once', touch_tip = True)
+    p10.distribute(13.5, sample_plate.columns_by_name()[samples[1]], [reaction_plate.wells_by_name()[i] for i in wells_reaction_woSAM],
+    disposal_volume = 0, blow_out = False, new_tip = 'once')
+    #p10.distribute(13.5, sample_plate.columns_by_name()[samples[1]], [reaction_plate.wells_by_name()[i] for i in wells_reaction_wSAM],
+    #disposal_volume = 0, blow_out = False, new_tip = 'once', touch_tip = True)
     
+    for i in range(0, len(wells_reaction_woSAM)):
+        p10.transfer(1.5, sample_plate.columns_by_name()[samples[2]], reaction_plate.wells_by_name()[wells_reaction_woSAM[i]], mix_after = (3, 1.5))
+
     # Distribute compound from 96-well library plate and mix
-    for i in range(0, len(cols_compounds)): # iterate through every column on the compound plate
-        lib_col = str(cols_compounds[i]) # column of the library plate (single number)
-        wells_target = [wells_reaction_woSAM[i], wells_reaction_wSAM[i]] # wells to distribute to (each compound is tested w/ and w/o SAM, so two apart columns)
-        p10.distribute(1.5, library_plate.columns_by_name()[lib_col], [reaction_plate.wells_by_name()[j] for j in wells_target],
-        new_tip = 'always', mix_after = (3, 1.5))
+    #for i in range(0, len(cols_compounds)): # iterate through every column on the compound plate
+    #    lib_col = str(cols_compounds[i]) # column of the library plate (single number)
+    #    wells_target = [wells_reaction_woSAM[i], wells_reaction_wSAM[i]] # wells to distribute to (each compound is tested w/ and w/o SAM, so two apart columns)
+    #    p10.distribute(1.5, library_plate.columns_by_name()[lib_col], [reaction_plate.wells_by_name()[j] for j in wells_target],
+    #    new_tip = 'always', mix_after = (3, 1.5))
